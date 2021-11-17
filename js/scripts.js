32feedda93d5777ce2ef3d3413d6f81db6663923
Dawid Wolas <vrplayerek@gmail.com>
2021-11-17 12:28:05 +0100

    add scripts

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+printMessage('Zagrałem'+ 'kamień '+'! Jeśli Twój ruch to '+'papier'+', to wygrywasz!');
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+var playerMove;
+playerMove = 'papier';
+printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to '+ playerMove +', to wygrywasz!');
