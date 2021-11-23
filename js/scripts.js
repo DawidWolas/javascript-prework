a626271d4e54cb4ee640233e9cfb5f449458c83b
Dawid Wolas <vrplayerek@gmail.com>
2021-11-23 17:41:18 +0100

    add computer move script

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
-printMessage('Zagrałem'+ 'kamień '+'! Jeśli Twój ruch to '+'papier'+', to wygrywasz!');
-var computerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 
-var playerMove;
-playerMove = 'papier';
-printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to '+ playerMove +', to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+}
+printMessage('Mój ruch: ' + computerMove);
